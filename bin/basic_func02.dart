2ff4e30f063c828fe8bff57cb6aba17178fb904e
Ubaydulloyev7780 <diyorubaydulloyev1@gmail.com>
2022-12-14 14:47:57 +0500

    8-version

--- bin/basic_func02.dart
+++ bin/basic_func02.dart
@@ -2,10 +2,10 @@
 //Create function arguments a and b of type int. 
 //Returns the floor division of a and b.
 double bulish(int a, int b){
-  int javob;
+  double javob;
   javob=a/b;
   return javob;
 }
 void main() {
-  print(bulish(3, 2));
+  print(bulish(4, 2));
 }
